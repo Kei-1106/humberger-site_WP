0935dae4aefad5d220642e42e6542d72511a03ce
Kei <keigyou.workword.1106@gmail.com>
2024-03-23 15:13:43 +0900

    cssの読み込み

--- header.php
+++ header.php
@@ -6,21 +6,13 @@
     <meta name="format-detection" content="telephone=no">
     <meta name="description" content="ハンバーガーサイト">
     <meta name="viewport" content="width=device-width, initial-scale">
-    <link rel="shortcut icon" href="img/favicon.ico">
-    <link rel="stylesheet" href="https://ajax.googleapis.com">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="scss/style.scss">
-    <link href="https://use.fontawesome.com/releases/v6.1.2/css/all.css" rel="stylesheet">
-    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_url(); ?>/img/favicon.ico">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,700;1,100&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700;900&display=swap" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
     <script src="js/main.js"></script>
+    <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
     <div class="u-display__flex u-position">
         <div class="u-display">
             <header class="l-header c-background-color__orange-light">
